5577ca7d8e6dbfe00e223f34f659915f84c4efd2
Krzysztof Tomczyk <251236@edu.p.lodz.pl>
2026-04-08 13:13:48 +0200

    Revert "Update DefaultCountingOutRhymer.java"
    This reverts commit 9f0b80731e6dfe5280c76edfe9a336c2fba92c1b.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,8 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
+    public static final int SIZE = 12;
     public static final int TOTAL = -1;
-    public static final int DEFAULT_VALUE = -1;
     private final int[] numbers = new int[SIZE];
 
     public int total = TOTAL;
@@ -15,7 +14,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == DEFAULT_VALUE;
+        return total == -1;
     }
 
     public boolean isFull() {
@@ -24,13 +23,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return DEFAULT_VALUE;
+            return -1;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return DEFAULT_VALUE;
+            return -1;
         return numbers[total--];
     }
 
